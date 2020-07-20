330464b3632387f7ee20e7bb250845af48a3f313
Marco Espinosa <marcoespinosa12@gmail.com>
2020-07-20 14:37:38 -0500

    create input field

---
+++ stories/Input/InputComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+interface IInputComponentProps {
+
+}
+
+export const InputComponent = (props: IInputComponentProps) => {
+  return (
+    <div>
+      ensuiteus
+    </div>
+  );
+}
