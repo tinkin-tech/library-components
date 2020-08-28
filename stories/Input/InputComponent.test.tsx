5ebc015a6004ea24163261fb18d64bdc303852f7
Marco <marco@tinkin.one>
2020-08-28 13:44:53 -0500

    delete comment test

--- stories/Input/InputComponent.test.tsx
+++ stories/Input/InputComponent.test.tsx
@@ -330,37 +330,4 @@ describe('InputComponent test', () => {
       expect(container.getElementsByTagName('textarea')).toHaveLength(1)
     })
   })
-  /*
-     it('Should be able to enter a value, when type is text', () => {
-     const dom = render(inputTypeText)
-     const input = getByTestId(dom.container, 'input-component')
-     fireEvent.change(input, { target: { value: 'Hello World!' } })
-     expect(input.getAttribute('value')).toBe('Hello World!')
-     })
-
-     it('The value should be empty when I enter text and type is number', () => {
-     const dom = render(inputTypeNumber)
-     const input = getByTestId(dom.container, 'input-component')
-     fireEvent.change(input, { target: { value: 'Hello World!' } })
-     expect(input.getAttribute('value')).toBe('')
-     })
-
-     it('Should pass value different from valueState of input component', () => {
-     const { getByTestId, rerender } = render(inputTypeText)
-     expect(getByTestId('input-component')).toMatchObject({ value: '' })
-     rerender(InputTypeTextWithValue)
-     expect(getByTestId('input-component')).toMatchObject({
-value: InputTypeTextWithValue.props.value,
-})
-})
-
-it('Should add className label-error when component pass with error', () => {
-const { getByTestId, rerender } = render(inputTypeText)
-expect(getByTestId('label-component').className).not.toContain(
-'label-error'
-)
-rerender(InputWithError)
-expect(getByTestId('label-component').className).toContain('label-error')
-})
-   */
 })
