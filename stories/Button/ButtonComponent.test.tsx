05e9f26f3dd9553de4d6dc52990b79bff796a3b1
Alejandro Ulloa <alejandro@tinkin.one>
2020-08-28 12:10:14 -0500

    functional disable property

--- stories/Button/ButtonComponent.test.tsx
+++ stories/Button/ButtonComponent.test.tsx
@@ -17,4 +17,13 @@ describe('render component <ButtonComponent />', () => {
     fireEvent.click(container.querySelector('button'))
     expect(fun).toHaveBeenCalledTimes(1)
   })
+
+  it('should recive disable property, functional disable', () => {
+    const fun = jest.fn()
+    const { container } = render(
+      <ButtonComponent buttonText="button" onClick={fun} disable={true} />
+    )
+    fireEvent.click(container.querySelector('button'))
+    expect(fun).toHaveBeenCalledTimes(0)
+  })
 })
--- stories/Button/ButtonComponent.tsx
+++ stories/Button/ButtonComponent.tsx
@@ -3,12 +3,13 @@ import * as React from 'react'
 interface IButtonComponent {
   buttonText: string
   onClick?: () => void
+  disable?: boolean
 }
 
 const ButtonComponent = (props: IButtonComponent): React.ReactElement => {
-  const { buttonText, onClick } = props
+  const { buttonText, onClick, disable } = props
 
-  return <button value={buttonText} onClick={onClick} />
+  return <button value={buttonText} onClick={disable ? null : onClick} />
 }
 
 export default ButtonComponent
