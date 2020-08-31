bc8a91fa4a56a6662815c0d1f1d329371abca0e1
Alejandro Ulloa <alejandro@tinkin.one>
2020-08-31 15:21:15 -0500

    values property

--- stories/Checkbox/CheckboxComponent.test.tsx
+++ stories/Checkbox/CheckboxComponent.test.tsx
@@ -18,10 +18,42 @@ const options = [
   },
 ]
 
+const values = [
+  {
+    id: 'id1',
+    label: 'value1',
+  },
+  {
+    id: 'id2',
+    label: 'value2',
+  },
+]
+
 describe('Checkbox component tests', () => {
-  it('should recive options list', () => {
-    const { container } = render(<CheckboxComponent options={options} />)
-    expect(container.getElementsByTagName('input')).toHaveLength(3)
-    expect(container.getElementsByTagName('label')).toHaveLength(3)
+  describe('options property', () => {
+    it('should recive options list', () => {
+      const { container } = render(<CheckboxComponent options={options} />)
+      expect(container.getElementsByTagName('input')).toHaveLength(3)
+      expect(container.getElementsByTagName('label')).toHaveLength(3)
+    })
+
+    it('should render with empty options', () => {
+      const { container } = render(<CheckboxComponent options={[]} />)
+      expect(container.getElementsByTagName('input')).toHaveLength(0)
+      expect(container.getElementsByTagName('label')).toHaveLength(0)
+    })
+  })
+
+  describe('values property', () => {
+    it('should recive values property with checked values', () => {
+      const { container } = render(
+        <CheckboxComponent options={options} values={values} />
+      )
+      expect(
+        Array.of(...container.getElementsByTagName('input')).filter(
+          (element) => element.checked === true
+        )
+      ).toHaveLength(2)
+    })
   })
 })
--- stories/Checkbox/CheckboxComponent.tsx
+++ stories/Checkbox/CheckboxComponent.tsx
@@ -7,17 +7,22 @@ type CheckboxOption = {
 
 interface CheckboxComponentPropsInterface {
   options: CheckboxOption[]
+  values?: CheckboxOption[]
 }
 
 const CheckboxComponent: React.FC<CheckboxComponentPropsInterface> = (
   props: CheckboxComponentPropsInterface
 ) => {
-  const { options } = props
+  const { options, values = [] } = props
   return (
     <>
       {options.map((option) => (
         <>
-          <input type="checkbox" id={option.id.toString()} />
+          <input
+            type="checkbox"
+            id={option.id.toString()}
+            checked={!!values.find((value) => value.id === option.id)}
+          />
           <label htmlFor={option.id.toString()}>{option.label}</label>
         </>
       ))}
