1ba6d6e2ce5bd4d20a65dfa4a403d424f194cf20
dennis <dennis@tinkin.one>
2020-09-21 09:11:38 -0500

    DRC-224 publish storibook on npm

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var ButtonComponent = require('./dist/stories/Button/ButtonComponent');
+var CheckboxComponent = require('./dist/stories/Checkbox/CheckboxComponent');
+var DateSelectorComponent = require('./dist/stories/DateSelector/DateSelectorComponent');
+var InputComponent = require('./dist/stories/Input/InputComponent');
+var MultilevelFilterComponent = require('./dist/stories/MultilevelFilter/MultilevelFilterComponent');
+var PaginationComponent = require('./dist/stories/Pagination/PaginationComponent');
+var RadioButtonComponent = require('./dist/stories/RadioButton/RadioButtonComponent');
+var SelectComponent = require('./dist/stories/Select/SelectComponent');
+var TableComponent = require('./dist/stories/Table/TableComponent');
+var TabsComponent = require('./dist/stories/Tabs/TabsComponent');
+var TooltipComponent = require('./dist/stories/Tooltip/TooltipComponent');
+var UploaderImageComponent = require('./dist/stories/UploaderImage/UploaderImageComponent');
+
+exports.ButtonComponent = ButtonComponent.default;
+exports.CheckboxComponent = CheckboxComponent.default;
+exports.DateSelectorComponent = DateSelectorComponent.default;
+exports.InputComponent = InputComponent.default;
+exports.MultilevelFilterComponent = MultilevelFilterComponent.default;
+exports.PaginationComponent = PaginationComponent.default;
+exports.RadioButtonComponent = RadioButtonComponent.default;
+exports.SelectComponent = SelectComponent.default;
+exports.TableComponent = TableComponent.default;
+exports.TabsComponent = TabsComponent.default;
+exports.TooltipComponent = TooltipComponent.default;
+exports.UploaderImageComponent = UploaderImageComponent.default;
