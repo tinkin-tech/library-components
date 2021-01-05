2e61da461ac49040d06a1c8f02725044a1da8226
Marco <marco@tinkin.one>
2021-01-05 17:15:16 -0500

    add new vars for accordion component and refact method

--- stories/Accordion/AccordionComponent.tsx
+++ stories/Accordion/AccordionComponent.tsx
@@ -18,48 +18,46 @@ const AccordionComponent = (
   const [openAccordionItems, changeOpenAccordionItems] = React.useState([
     defaultKeyOpen,
   ])
+  const indexIsIncludesOnAccordionItems = (index: number): boolean =>
+    openAccordionItems.includes(index)
   const handleShowContentOfAccordionItem = (index: number): void => {
-    const NEGATIVE_INDEX = -1
-    const openAccordionItemsCopy = [...openAccordionItems]
-    const indexOfItem = openAccordionItems.indexOf(index)
-    if (indexOfItem === NEGATIVE_INDEX) {
-      openAccordionItemsCopy.push(index)
-      changeOpenAccordionItems(openAccordionItemsCopy)
-    } else {
-      changeOpenAccordionItems(
-        openAccordionItemsCopy.filter((item) => item !== index)
-      )
-    }
+    changeOpenAccordionItems(
+      indexIsIncludesOnAccordionItems(index)
+        ? openAccordionItems.filter((item) => item !== index)
+        : [...openAccordionItems, index]
+    )
   }
   return (
     <div className="accordion-component">
       {accordionList.map((accordion, index) => (
         <div
-          className={`accordion-item p-b-lg ${
-            openAccordionItems.includes(index) ? 'open' : ''
+          className={`accordion-item ${
+            indexIsIncludesOnAccordionItems(index) ? 'open' : ''
           }`}
           key={index}
         >
           <a
-            className="cursor-pointer bg-white flex-row flex-center cursor-pointer color-primary toggle-switch"
+            className="cursor-pointer flex-row flex-center cursor-pointer toggle-switch"
             onClick={(): void => handleShowContentOfAccordionItem(index)}
           >
             <div className="icon-content">
               <span
                 className={`${
-                  openAccordionItems.includes(index)
+                  indexIsIncludesOnAccordionItems(index)
                     ? 'icon-arrow-top'
                     : 'icon-arrow-down'
-                } text-primary`}
+                }`}
               />
             </div>
             <div className="toggle-text">
-              {openAccordionItems.includes(index)
+              {indexIsIncludesOnAccordionItems(index)
                 ? accordion.openLabel
                 : accordion.closeLabel}
             </div>
           </a>
-          {openAccordionItems.includes(index) && <div>{accordion.content}</div>}
+          {indexIsIncludesOnAccordionItems(index) && (
+            <div>{accordion.content}</div>
+          )}
         </div>
       ))}
     </div>
